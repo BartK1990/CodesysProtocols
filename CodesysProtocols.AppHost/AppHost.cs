b158a1b2bc512309400676267e243de642c091ab
Bartosz Kuriata <kuriata.bartosz@gmail.com>
2026-01-06 22:44:32 +0100

    Added docker publish action

--- CodesysProtocols.AppHost/AppHost.cs
+++ CodesysProtocols.AppHost/AppHost.cs
@@ -1,5 +1,25 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.CodesysProtocols_Blazor>("codesysprotocols-blazor");
+builder.AddDockerComposeEnvironment("compose")
+    .WithProperties(env =>
+    {
+        env.DefaultNetworkName = "codesysprotocols_net";
+    })
+    .WithDashboard(dashboard =>
+    {
+        dashboard.WithHostPort(8080)
+                .WithForwardedHeaders(enabled: true);
+    });
+
+var database = builder.AddSqlServer("database")
+    .AddDatabase("CodesysProtocols");
+
+builder.AddProject<Projects.CodesysProtocols_Blazor>("codesysprotocols-blazor")
+    .WithReference(database)
+    .WaitFor(database)
+    .PublishAsDockerComposeService((serviceResource, service) => 
+    {
+        service.ContainerName = serviceResource.Name;
+    });
 
 builder.Build().Run();
